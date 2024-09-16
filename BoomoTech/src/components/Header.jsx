32feb57887b935ef8efeec58ebd6bec50a1b7a26
Farzad Sanjarani <farzadsanjarani3@gmail.com>
2024-09-16 12:26:12 +1000

    main sections

--- BoomoTech/src/components/Header.jsx
+++ BoomoTech/src/components/Header.jsx
@@ -1,7 +1,8 @@
 import { useLocation } from "react-router-dom";
 import { disablePageScroll, enablePageScroll } from "scroll-lock";
+
+import { brainwave } from "../assets";
 import { navigation } from "../constants";
-import boomotechSVG from "../assets/boomotechSVG2.svg";
 import Button from "./Button";
 import MenuSvg from "../assets/svg/MenuSvg";
 import { HamburgerMenu } from "./design/Header";
@@ -30,13 +31,13 @@ const Header = () => {
 
   return (
     <div
-      className={`fixed top-0 left-0 w-full z-50 border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
+      className={`fixed top-0 left-0 w-full z-50  border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
         openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
       }`}
     >
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
         <a className="block w-[12rem] xl:mr-8" href="#hero">
-          <img src={boomotechSVG} width={190} height={40} alt="boomotechSVG" />
+          <img src={brainwave} width={190} height={40} alt="Brainwave" />
         </a>
 
         <nav
