890ae53d1042405a8525569c87750e1c329aa45f
Farzad Sanjarani <farzadsanjarani3@gmail.com>
2024-12-19 18:12:49 +1000

    f1

--- BoomoTech/src/App.jsx
+++ BoomoTech/src/App.jsx
@@ -1,6 +1,6 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
-// import Benefits from "./components/Benefits";
-// import Collaboration from "./components/Collaboration";
+import Benefits from "./components/Benefits";
+import Collaboration from "./components/Collaboration";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
@@ -14,12 +14,12 @@ const App = () => {
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
         <Hero />
-        {/* <Benefits />
+        <Benefits />
         <Collaboration />
         <Services />
         <Pricing />
         <Roadmap />
-        <Footer /> */}
+        <Footer />
       </div>
 
       <ButtonGradient />
